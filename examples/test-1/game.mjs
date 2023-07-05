18b7e17e7c99c72c79e9124e47e80f1be271a4c8
Ben Coleman <benc.uk@gmail.com>
2023-07-05 18:34:54 +0100

    🛞 Good stuff

--- examples/test-1/game.mjs
+++ examples/test-1/game.mjs
@@ -6,27 +6,35 @@ const ctx = await Context.init('canvas')
 
 const tableMdl = await Model.parse('../objects', 'table.obj')
 const chestMdl = await Model.parse('../objects', 'chest.obj')
-const blockMdl = await Model.parse('../objects', 'block.obj')
+const blockMdl = await Model.parse('../objects', 'floor.obj')
 ctx.models.add(tableMdl)
 ctx.models.add(chestMdl)
 ctx.models.add(blockMdl)
 
-const c1 = ctx.createInstance('table')
-c1.position = [5, 3, 0]
-c1.scale = [2.1, 1.3, 2.7]
-c1.rotateX(Math.PI / 2)
+const table = ctx.createInstance('table')
+table.position = [5, 3, -4]
+table.scale = [2.1, 1.3, 2.7]
+table.rotateX(Math.PI / 2)
 
-const c2 = ctx.createInstance('chest')
-c2.position = [-10, 0, 0]
+const chest1 = ctx.createInstance('chest')
+chest1.position = [-10, 0, 0]
 
-const floor = ctx.createInstance('block')
-floor.scale = [10, 1, 10]
+const chest2 = ctx.createInstance('chest')
+chest2.position = [13, 0, 11]
+chest2.rotateY(Math.PI / 1.5)
+
+const floor = ctx.createInstance('floor')
+floor.scale = [8, 1, 8]
 floor.rotateX(Math.PI / 2)
 floor.position = [0, -10, 0]
 
-ctx.camera.position = [11, 24, 30]
+const camHeight = 30
+ctx.camera.position = [11, camHeight, 30]
 ctx.camera.lookAt = [0, 0, 0]
-ctx.camera.far = 345
+ctx.camera.far = 300
+
+// Static light
+ctx.defaultLight.position = [40, 22, 14]
 
 let time = 0
 const radius = 40
@@ -37,8 +45,11 @@ ctx.update = (delta) => {
 
   const x = Math.cos(time / speed) * radius
   const z = Math.sin(time / speed) * radius
-  ctx.camera.position = [x, 24, z]
-  ctx.defaultLight.position = [x, 20, z]
+  ctx.camera.position = [x, camHeight, z]
+
+  const xLook = Math.cos(time / speed + Math.PI) * radius
+  ctx.camera.lookAt = [xLook, -5, 0]
+  //ctx.defaultLight.position = [x, camHeight, z]
 }
 
 ctx.debug = true
