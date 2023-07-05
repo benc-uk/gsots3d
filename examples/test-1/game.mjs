10513a7f8c8ddcab1f8ee96a2b2693d31cc61224
Ben Coleman <benc.uk@gmail.com>
2023-07-05 16:38:07 +0100

    🏠 add example game

---
+++ examples/test-1/game.mjs
@@ -0,0 +1,42 @@
+import { Model, Context, setLogLevel } from '../../browser/gsots3d.js'
+
+setLogLevel('debug')
+
+const ctx = await Context.init('canvas')
+
+const tableMdl = await Model.parse('../objects', 'table.obj')
+const chestMdl = await Model.parse('../objects', 'chest.obj')
+ctx.models.add(tableMdl)
+ctx.models.add(chestMdl)
+
+const c1 = ctx.createInstance('table')
+c1.position = [5, 4, 0]
+c1.scale = [2.1, 2.1, 2.7]
+c1.rotateX(Math.PI / 2)
+
+const c2 = ctx.createInstance('chest')
+c2.position = [-10, 0, 0]
+
+const floor = ctx.createInstance('table')
+floor.scale = [10, -4, 10]
+// floor.rotateX(Math.PI / 2)
+
+ctx.camera.position = [11, 24, 30]
+ctx.camera.lookAt = [0, 0, 0]
+ctx.camera.far = 345
+
+let time = 0
+const radius = 40
+const speed = 1.6
+
+ctx.update = (delta) => {
+  time += delta
+
+  const x = Math.cos(time / speed) * radius
+  const z = Math.sin(time / speed) * radius
+  ctx.camera.position = [x, 24, z]
+  ctx.defaultLight.position = [x, 24, z]
+}
+
+ctx.debug = true
+ctx.start()
