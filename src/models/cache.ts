b53507fd56959a5fb8914d10ab2e049a4bd39ece
Ben Coleman <benc.uk@gmail.com>
2023-07-10 03:33:04 +0100

    Sigh

--- src/models/cache.ts
+++ src/models/cache.ts
@@ -12,6 +12,10 @@ import { Model } from './model.ts'
 export class ModelCache {
   private cache = new Map<string, Model>()
 
+  /**
+   * Return a model from the cache by name
+   * @param name
+   */
   get(name: string) {
     if (!this.cache.has(name)) {
       throw new Error(`Model ${name} not found in cache`)
@@ -20,6 +24,9 @@ export class ModelCache {
     return this.cache.get(name)
   }
 
+  /**
+   * Add a model to the cache
+   */
   add(model: Model) {
     log.debug(`🧰 Adding model '${model.name}' to cache`)
 
