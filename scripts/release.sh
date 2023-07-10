52ff2e9d06c3cc158ead2cc3ec99b0dd5cc00f43
Ben Coleman <benc.uk@gmail.com>
2023-07-10 02:59:48 +0100

    0.0.1

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-
 set -e
 
-TYPE=${1:-minor}
+VERSION_TYPE=${1:-patch}
+
+# Prompt user for confirmation
+read -p "### 🚀 Releasing new version. Are you sure? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  exit 1
+fi
+
+# Check if the current branch is master
+if [[ $(git branch --show-current) != "main" ]]; then
+  echo "### 🚨 Please checkout the main branch before releasing a new version"
+  exit 1
+fi
 
 # Check if there are any uncommitted changes
 if [[ $(git status -s) ]]; then
@@ -10,15 +22,16 @@ if [[ $(git status -s) ]]; then
   exit 1
 fi
 
-echo "### 🎈 Releasing new $TYPE version"
+# We do things this way so dist-bundle is in the repo, BEFORE we create a new tag
+
+TAG=$(npm version "$VERSION_TYPE" --no-git-tag-version)
+
+echo "### 🎈 Releasing new version ${TAG}"
 echo "### 🔨 Building browser ESM bundle"
 
 npm run build-bundle
 git add dist-bundle
 git commit -m "📦 Build browser ESM bundle for release"
 
-npm version "$TYPE"
-tag=$(git tag --points-at HEAD)
-
-echo "### 🚀 Released version is ${tag}"
-git push origin "$tag"
+git tag "$TAG"
+git push origin "$TAG"
