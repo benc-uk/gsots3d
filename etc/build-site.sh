08d51104bab0dd77399997bb320bac29758c74dd
Ben Coleman <benc.uk@gmail.com>
2023-07-15 15:59:34 +0100

    🐞 Fix GH pages deploy

--- etc/build-site.sh
+++ etc/build-site.sh
@@ -4,10 +4,12 @@ npm run docs
 
 rm -rf ./site
 mkdir -p ./site
-cp -r ./etc/site/* ./site
-showdown makehtml -i readme.md -o site/index.html --flavor github --append
-echo "</article></body></html>" >> site/index.html
 
 cp -r ./docs ./site
 cp -r ./examples ./site
 cp -r ./dist-bundle ./site
+
+cp -r ./etc/site/* ./site
+showdown makehtml -i readme.md -o site/index.html --flavor github --append
+echo "</article></body></html>" >> site/index.html
+
