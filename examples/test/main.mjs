79b4967e13bb9ff2a9e35950c62333a019b377f7
Ben Coleman <benc.uk@gmail.com>
2023-07-19 01:24:05 +0100

    🐛 Fix demos

--- examples/test/main.mjs
+++ examples/test/main.mjs
@@ -1,18 +1,17 @@
-import { Context, LightPoint, Material, RGB_BLACK } from '../../dist-bundle/gsots3d.js'
+import { Context, Material } from '../../dist-bundle/gsots3d.js'
 
 const ctx = await Context.init()
 window.addEventListener('resize', () => ctx.resize())
 ctx.debug = true
 
-ctx.camera.position = [0, 900, 500]
-ctx.globalLight.setAsPosition(200, 1, 0)
-ctx.globalLight.colour = RGB_BLACK
+ctx.camera.position = [0, 1000, 800]
+ctx.globalLight.setAsPosition(30, 1, 0)
 ctx.camera.far = 5000
 
-const m = Material.WHITE //createBasicTexture('../../_textures/stone-wall.png')
+const m = Material.createBasicTexture('../_textures/stone-wall.png')
 m.specular = [0, 0, 0]
 
-const _ = ctx.createPlaneInstance(m, 1000, 1000, 1, 1, 5)
+ctx.createPlaneInstance(m, 1000, 1000, 1, 1, 5)
 
 const colortable = [
   [1, 0, 0],
@@ -23,13 +22,9 @@ const colortable = [
   [0, 1, 1],
 ]
 
-const maxlights = 2
+const maxlights = 6
 for (let i = 0; i < maxlights; i++) {
-  const light = new LightPoint()
-  light.position = [0, 60, 200 + i * -500]
-  light.colour = colortable[i % colortable.length]
-
-  ctx.lights.push(light)
+  ctx.createPointLight([0, 40, 400 - i * 150], colortable[i], 10)
 }
 
 ctx.start()
