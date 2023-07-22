a4a4d0165b5a99bcc470d90291f539efa840674c
Ben Coleman <benc.uk@gmail.com>
2023-07-22 08:45:33 +0100

    📦 Releasing new version v0.0.1

--- dist-bundle/gsots3d.js
+++ dist-bundle/gsots3d.js
@@ -283,7 +283,7 @@ function setLogLevel(level) {
 }
 
 // package.json
-var version = "0.0.1-alpha.7";
+var version = "0.0.1";
 
 // node_modules/twgl.js/dist/5.x/twgl-full.module.js
 var VecType = Float32Array;
